b986fa3d88657d291232051d187586efd18a5a4a
Doug <drehnstrom@gmail.com>
2018-01-02 11:52:13 -0500

    Added App Engine Support

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    model = {"title":"Welcome Kubernetes Fans to our great program."}
+    model = {"title":"Welcome App Engine Fans to our great program."}
     return render_template('index.html', model=model)
 
 
