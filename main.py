c25ece2d5bdad4a51f8ecc0a84b381f115a95fd7
drehnstrom <drehnstrom@gmail.com>
2020-05-14 17:47:25 -0400

    Tempe

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    model = {"title": "Hello GCP!"}
+    model = {"title": "Hello Tempe, AZ!"}
     return render_template('index.html', model=model)
 
 
