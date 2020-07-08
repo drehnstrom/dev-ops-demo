8a83514ef4496d3f32b55f18834a4460606fa476
drehnstrom <drehnstrom@gmail.com>
2020-07-08 14:45:32 -0400

    Pitt

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    model = {"title": "Hello Lions "}
+    model = {"title": "Go Pitt beat Penn State!"}
     return render_template('index.html', model=model)
 
 
