c56d7e3d992905e94ed0d0e04932e971c1205f1b
drehnstrom <drehnstrom@gmail.com>
2020-09-15 13:21:56 +0000

    Changed Message

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    model = {"title": "Go Pitt beat Penn State!"}
+    model = {"title": "Hello World!"}
     return render_template('index.html', model=model)
 
 
