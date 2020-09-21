a1625525b339ed89ed22cdbb879fd76d4ad4d11c
drehnstrom <drehnstrom@gmail.com>
2020-09-21 15:53:38 +0000

    Changed Something

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    model = {"title": "Hello Two Sigma!"}
+    model = {"title": "Hello Two Sigma!!!"}
     return render_template('index.html', model=model)
 
 
