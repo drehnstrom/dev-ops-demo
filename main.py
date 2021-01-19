b1b6a86958e022be30a19a3de7b4d53fe857b428
drehnstrom <drehnstrom@users.noreply.github.com>
2021-01-19 11:01:21 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    model = {"title": "Hello PANW!"}
+    model = {"title": "Hello World!"}
     return render_template('index.html', model=model)
 
 
