0a7e8b9ef60513e33f88f458a08f055aff069479
drehnstrom <drehnstrom@gmail.com>
2021-03-10 12:57:08 +0000

    Changes

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    model = {"title": "Hello Double Verify"}
+    model = {"title": "Hello Double Verify!"}
     return render_template('index.html', model=model)
 
 
